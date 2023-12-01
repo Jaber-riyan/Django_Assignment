0e7cd51d2b3900ea3ae273095a6f8ec818cd1fea
Jaber Riyan <129547732+Jaber-riyan@users.noreply.github.com>
2023-12-01 20:57:43 +0600

    Delete Assignment_3 directory

--- Assignment_3/meal/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('showitem/',views.show_item, name='showitem'),
-    path('aboutus/',views.about_us,name="aboutus"),
-    path('buynow/',views.buy_now,name="buynow"),
-]
