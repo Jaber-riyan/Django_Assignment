09fe43ab88031cfc475fb01ec6e149b37a5d2084
Jaber Riyan <129547732+Jaber-riyan@users.noreply.github.com>
2023-12-21 15:35:46 +0600

    Delete mid_term directory

--- mid_term/car/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . import models
-
-# Register your models here.
-admin.site.register(models.CarModel)
-admin.site.register(models.Comment)
-admin.site.register(models.Cart)
--- mid_term/car_brand/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . import models
-
-# Register your models here.
-class CarBrandAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('brand_name',)}
-    list_display = ['brand_name','slug',]
-
-admin.site.register(models.CarBrand,CarBrandAdmin)
